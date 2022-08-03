07f70ef590e9769786166a29ceaa146d0a73160c
FujimuraShuhei <shushu2525.1694@gmail.com>
2022-08-03 23:33:18 +0900

    ♻️ カスタムフック化を行った

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,44 +3,13 @@ import { useCallback, useState } from 'react';
 import Footer from '../components/Footer/Footer';
 import { Header } from '../components/Header/Header';
 import Main from '../components/Main/Main';
+import { useCounter } from '../hooks/useCounter';
+import { useInputArray } from '../hooks/useInputArray';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
-  const [count, setCount] = useState(1);
-  const [text, setText] = useState('');
-  const [isShow, setIsShow] = useState(true);
-  const [array, setArray] = useState([]);
-
-  const handleClick = useCallback(() => {
-    if (count < 10) {
-      setCount((prevCount) => prevCount + 1);
-    }
-  }, [count]);
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 10) {
-      alert('10文字以内にしてください');
-      return;
-    }
-    setText(e.target.value);
-  }, []);
-
-  const handleDisplay = useCallback(() => {
-    if (count < 10) {
-      setIsShow((prevIsShow) => !prevIsShow);
-    }
-  }, [count]);
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some((item) => item === text)) {
-        alert("同じ要素が既に存在します");
-        return prevArray;
-      }
-      const newArray = [...prevArray, text];
-      return newArray;
-    });
-  }, [text]);
+  const { count, isShow, handleClick, handleDisplay } = useCounter();
+  const { text, array, handleChange, handleAdd } = useInputArray();
 
   return (
     <div className={styles.container}>
@@ -49,17 +18,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <div className="kinou">
+
         {isShow ? <h1>{count}</h1> : null}
         <button onClick={handleClick}>ボタン</button>
         <button onClick={handleDisplay}>{isShow ? '非表示' : '表示'}</button>
-        <a
-          onClick={function (e) {
-            alert(123);
-          }}
-        >
-          ボタン
-        </a>
+
         <button onClick={handleAdd}>追加</button>
         <input type="text" value={text} onChange={handleChange} />
         <ul>
@@ -67,7 +30,6 @@ export default function Home() {
             return <li key={item}>{item}</li>;
           })}
         </ul>
-      </div>
       <Main page="index" />
       <Footer />
     </div>

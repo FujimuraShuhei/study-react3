727ea289c41df528f70f0f96c262ee3ec499c64c
FujimuraShuhei <shushu2525.1694@gmail.com>
2022-08-04 09:16:09 +0900

    ✨ ページ間でのusestateのリフトアップ

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,14 @@
+import { useCounter } from '../hooks/useCounter';
+import { useInputArray } from '../hooks/useInputArray';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+   const counter = useCounter();
+  const inputArray = useInputArray();
+  
+  // { count, isShow, handleClick, handleDisplay }
+  // { text, array, handleChange, handleAdd }
+  return <Component {...pageProps} {...counter} {...inputArray} />
 }
 
 export default MyApp
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,11 @@
 import Head from 'next/head';
-import { useCallback, useState } from 'react';
 import Footer from '../components/Footer/Footer';
 import { Header } from '../components/Header/Header';
 import Main from '../components/Main/Main';
-import { useCounter } from '../hooks/useCounter';
-import { useInputArray } from '../hooks/useInputArray';
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
-  const { count, isShow, handleClick, handleDisplay } = useCounter();
-  const { text, array, handleChange, handleAdd } = useInputArray();
+export default function Home(props) {
+  const { count, isShow, handleClick, handleDisplay, text, array, handleChange, handleAdd } = props;
 
   return (
     <div className={styles.container}>
@@ -19,17 +15,17 @@ export default function Home() {
       </Head>
       <Header />
 
-        {isShow ? <h1>{count}</h1> : null}
-        <button onClick={handleClick}>ボタン</button>
-        <button onClick={handleDisplay}>{isShow ? '非表示' : '表示'}</button>
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? '非表示' : '表示'}</button>
 
-        <button onClick={handleAdd}>追加</button>
-        <input type="text" value={text} onChange={handleChange} />
-        <ul>
-          {array.map((item) => {
-            return <li key={item}>{item}</li>;
-          })}
-        </ul>
+      <button onClick={handleAdd}>追加</button>
+      <input type="text" value={text} onChange={handleChange} />
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
       <Main page="index" />
       <Footer />
     </div>

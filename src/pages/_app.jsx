fcff180a1aeeedec508ea3835efe054e64531510
FujimuraShuhei <shushu2525.1694@gmail.com>
2022-08-05 10:09:12 +0900

    ✨ useRouterを使ってみました

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,12 @@
+import { useBgColor } from '../hooks/useBgColor';
 import { useCounter } from '../hooks/useCounter';
 import { useInputArray } from '../hooks/useInputArray';
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-   const counter = useCounter();
+const MyApp = ({ Component, pageProps }) => {
+  const counter = useCounter();
   const inputArray = useInputArray();
+  useBgColor();
   
   // { count, isShow, handleClick, handleDisplay }
   // { text, array, handleChange, handleAdd }
